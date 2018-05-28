6e19f70e4afe0e6c6d11adf2144573dea4a39122
Angelo Cavallet <angelocavallet@gmail.com>
2018-05-28 15:12:26 -0300

    Update localconfig.js

--- localconfig.js
+++ localconfig.js
@@ -2,9 +2,9 @@ exports.LocalConfig = function (localizer, project) {
 
     localizer.where('center').then([29.9377, -3.4216, 9]);
     localizer.where('Layer').if({'Datasource.type': 'postgis'}).then({
-        'Datasource.dbname': 'hdm',
-        'Datasource.password': 'chp@0479',
-        'Datasource.user': 'angelo',
+        'Datasource.dbname': 'gis',
+        'Datasource.password': '',
+        'Datasource.user': 'osm',
         'Datasource.host': 'localhost'
     });
 };
